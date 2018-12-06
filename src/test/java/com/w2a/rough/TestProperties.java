50b52dad730b96b89dad4f377da5ca4b17dfd5d5
unknown <gmborgjoseph@gmail.com>
2018-12-06 13:28:15 +0530

    Initial Commit

---
+++ src/test/java/com/w2a/rough/TestProperties.java
@@ -0,0 +1,25 @@
+package com.w2a.rough;
+
+import java.io.IOException;
+
+public class TestProperties {
+
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * System.out.println(System.getProperty("user.dir")); Properties config = new
+		 * Properties(); Properties OR = new Properties(); FileInputStream fis = new
+		 * FileInputStream(System.getProperty("user.dir")+
+		 * "\\src\\test\\resources\\properties\\Config.properties"); config.load(fis);
+		 * 
+		 * fis = new FileInputStream(System.getProperty("user.dir")+
+		 * "\\src\\test\\resources\\properties\\OR.properties"); OR.load(fis);
+		 * 
+		 * System.out.println(config.getProperty("browser"));
+		 * 
+		 * //driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
+		 * System.out.println(OR.getProperty("bmlBtn"));
+		 */
+	}
+
+}
---
+++ src/test/java/com/w2a/rough/TestTimeStamp.java
@@ -0,0 +1,15 @@
+package com.w2a.rough;
+
+public class TestTimeStamp {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Date d= new Date(); String screenshotName = d.toString().replace(":",
+		 * "_").replace(" ", "_")+".jpg"; System.out.println(screenshotName);
+		 * System.out.println(d);
+		 */
+
+	}
+
+}
